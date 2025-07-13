40d1c2cafc89b190083b15e808e2afe78d392d36
barrak7 <hamzabarrak83@gmail.com>
2025-07-13 13:53:55 +0100

    implemented __matmul__ and its backward function

--- np_grad.py
+++ np_grad.py
@@ -1,2 +1,63 @@
-from numpy import numpy.ndarray as ndarray
+from numpy import ndarray
+import numpy as np
 
+class np_grad(ndarray):
+    """
+        np_grad: the numpy array wrapper that provides reverse mode auto diff.
+    """
+    def __new__(cls, value, *args, **kwargs):
+        """
+            Creates a new instance of the class.
+
+            Since np.ndarray doesn't have an __init__ function,
+            I had to override __new__ as a workaround to initializing
+            the parent instance with the same value.
+        """
+        arr = np.array(value)
+        return super(np_grad, cls).__new__(cls, arr.shape, dtype=np.float32)
+    
+    def __init__(self, value: ndarray, _children=(), _op=''):
+        """
+            Parameters
+            ----------
+            value: ndarray
+                The matrix.
+            _children: tuple[Any, Any] | tuple[]
+                default: ()
+                The operands that resulted in value.
+            _op: str
+                default: '' 
+                The operation that resulted in value.
+            _grad: ndarray.float
+                default: 0
+                The gradient of the current node.
+            _backward: Callable[[out_grad], None]
+                default: None
+                The _backward callable that performs the chain rule of the output gradient against _children.
+        """
+        self[:] = np.array(value, dtype=np.float32)
+        self._children = _children
+        self._op = _op
+        self._backward = lambda: None
+        self._grad = np.zeros_like(self)
+
+    def __matmul__(self, other):
+        """
+            Overrides the @ operator. Performs matrix multiplication between self and other.
+
+            Calls numpy matmul on the ndarrays. Uses the output value to create a new np_grad object.
+
+            Defines the _backward function and sets it in the newly created object.
+
+            Returns:
+                np_grad, the result of the operation.
+        """
+        out = super(np_grad, self).__matmul__(other)
+        out = np_grad(out, (self, other), '@')
+
+        def _backward(out_grad):
+            self._grad += out_grad @ other.T
+            other._grad += self.T @ out_grad
+        
+        out._backward = _backward
+        return out
