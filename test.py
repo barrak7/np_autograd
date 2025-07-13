0904db981960021889486b85c6142c8b79975a96
barrak7 <hamzabarrak83@gmail.com>
2025-07-13 13:54:19 +0100

    tested np_grad matmul against pytorch

---
+++ test.py
@@ -0,0 +1,24 @@
+from np_grad import np_grad
+import torch
+import numpy as np
+
+
+a = np_grad(np.random.randint(0,9,(3,2)))
+b = np_grad(np.random.randint(0,9,(2,5)))
+
+a_t = torch.tensor(a, requires_grad=True)
+b_t = torch.tensor(b, requires_grad=True)
+
+out = a @ b
+
+out._backward(np.ones_like(out))
+
+print(a._grad)
+print(b._grad)
+
+out = a_t @ b_t
+
+out.backward(torch.ones_like(out))
+
+print(a_t.grad)
+print(b_t.grad)
